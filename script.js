ec6ca305ee9bbbc4045fae566f35e11545386f7d
AlanDEvCoelho <101427195+AlanDEvCoelho@users.noreply.github.com>
2022-03-11 16:42:33 -0300

    meu primeiro commit
    Esse é o meu primeiro commit estou enviando os arquivos da minha aplicação que eu fiz durante  a semana Mission One

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+let botao = document.getElementById("botao")
+let select = document.getElementById("select-moedas")
+
+
+
+
+async function converterMoedas() {
+
+    let moedas = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL").then(function (resposta) {
+        return resposta.json()
+    })
+
+    let dolar = moedas.USDBRL.high
+    let euro = moedas.EURBRL.high
+
+    console.log(dolar)
+    console.log(euro)
+
+
+    let inputValorEmReais = Number(document.getElementById("input").value)
+    let inputMoedas = document.getElementById("input-moedas")
+    let textoReal = document.getElementById("texto-real")
+
+    if (select.value === "US$ - Dólar Americano") {
+        let valorEmDolares = inputValorEmReais / dolar
+        inputMoedas.innerHTML = valorEmDolares.toLocaleString("en-US", { style: "currency", currency: "USD" })
+    }
+
+    if (select.value === "€ - Euro") {
+        let valoremEuros = inputValorEmReais / euro
+        inputMoedas.innerHTML = valoremEuros.toLocaleString("de-DE", { style: "currency", currency: "EUR" })
+    }
+
+
+    textoReal.innerHTML = inputValorEmReais.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+
+}
+// Essa funcao e responsavel por trocar a bandeira e as moedas
+function trocaDeMoeda() {
+    let textoMoedas = document.getElementById("texto-moedas")
+    let bandeiraMoedas = document.getElementById("bandeira-moedas")
+
+    if (select.value === "US$ - Dólar Americano") {
+        textoMoedas.innerHTML = "Dólar Americano"
+        bandeiraMoedas.src = "./img/eua.png"
+
+    }
+
+
+    if (select.value === "€ - Euro") {
+        textoMoedas.innerHTML = "Euro"
+        bandeiraMoedas.src = "./img/euro.png"
+    }
+
+    converterMoedas()
+}
+
+
+botao.addEventListener("click", converterMoedas)
+select.addEventListener("change", trocaDeMoeda)
+
+
+
